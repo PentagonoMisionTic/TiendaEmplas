04d7c9f65d72932f771b0c3877d5d1560abdc184
masteralex <110934776+alexrinve@users.noreply.github.com>
2022-10-26 21:21:32 -0500

    home

--- back/routes/products.js
+++ back/routes/products.js
@@ -3,10 +3,10 @@ const { getProducts, newProduct, getProductById, updateProduct, deleteProduct } 
 const router=express.Router();
 
 
-router.route('/productos').get(getProducts)//establecemos ruta del get
-router.route('/producto/nuevo').post(newProduct)//establecemos la ruta del producto nuevo creado
-router.route('/producto/:id').get(getProductById)//ruta para consultar producto por id
-router.route('/producto/:id').put(updateProduct);//Establecemos la ruta editar
+router.route('/productos').get(getProducts)//implementamos ruta del get
+router.route('/producto/nuevo').post(newProduct)//Construimos  la ruta del producto nuevo creado
+router.route('/producto/:id').get(getProductById)//Consultamos por medio de una ruta el  producto usando un  id
+router.route('/producto/:id').put(updateProduct);//elaboramos  la ruta editar
 router.route('/producto/:id').delete(deleteProduct);//Establecemos la ruta eliminar
 
 module.exports=router;
