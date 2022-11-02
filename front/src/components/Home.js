060513a21aed6de2d592ac877d4855b1a2e5aa81
Yulieth Montes <yulieth_montes_1126@hotmail.com>
2022-11-01 22:07:46 -0500

    1/nov
    Codigo

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -22,7 +22,7 @@ export const Home = () => {
 
     return (
         <Fragment>
-            {loading ? <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i> :(
+            {loading ? <i className="fa fa-refresh fa-spin fa-3x fa-fw"></i> :(
                 <Fragment>
                     <MetaData title="EMPLAS S.A. "></MetaData>
             <h1 id="encabezado_productos">Ultimos Productos</h1>
--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
             <nav className='navbar row'>
                 <div className='col-12 col-md-3'>
                     <div className='navbar-brand'>
-                        <img src="./images/logoEmplas.png" alt="Tienda Emplas logo"></img>
+                        <img src="./images/logo192.png" alt="Tienda Emplas logo"></img>
                     </div>
                 </div>
 
@@ -16,20 +16,20 @@ const Header = () => {
                         <input
                             type="text"
                             id="search_field"
-                            class="form-control"
+                            className="form-control"
                             placeholder='Encuentra aqui todo nuestro catálogo!'></input>
-                        <div class="input-group-append">
-                            <button id="search-btn" class="btn">
-                                <i class="fa fa-search-plus fa-2x text-white" aria-hidden="true"></i>
+                        <div className="input-group-append">
+                            <button id="search-btn" className="btn">
+                                <i className="fa fa-search-plus fa-2x text-white" aria-hidden="true"></i>
                             </button>
                         </div>
                     </div>
                 </div>
 
                 <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-                    <span><button type="button" class="btn btn-warning">Inicie Sesión</button></span>
+                    <span><button type="button" className="btn btn-warning">Inicie Sesión</button></span>
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <span><i class="fa fa-shopping-cart fa-2x text-white" aria-hidden="true"></i></span>
+                    <span><i className="fa fa-shopping-cart fa-2x text-white" aria-hidden="true"></i></span>
                     &nbsp;
                     <span className="ml-1" id="cart_count">2 </span>
                 </div>
